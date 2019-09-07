bd5b7620f00d145a0545f46a9281672f073fc8fc
unknown <mfjotaa@hotmail.com>
2019-09-07 17:12:30 +0200

    log

--- grandpyapp/__init__.py
+++ grandpyapp/__init__.py
@@ -1,5 +1,2 @@
 from flask import Flask
 from .views import app
-app = Flask(__name__)
-app.add_url_rule('/get_word', view_func=views.get_prediction)
-app.add_url_rule('/get_coord', view_func=views.get_coordinates)
