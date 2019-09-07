c9844333dee95894596b55fc62aed1d12ec04216
unknown <mfjotaa@hotmail.com>
2019-09-07 14:55:58 +0200

    corrections

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
  worker: python run.py
  web: gunicorn grandpyapp:app
-# init: FLASK_APP=run.py flask init_db
--- grandpyapp/static/js/grandpy.js
+++ grandpyapp/static/js/grandpy.js
@@ -1,13 +1,9 @@
 // Management of the end of the loading of the web page
 window.addEventListener("load", function () {
-    /*console.log("Page entièrement chargée");*/
-  
     var boutonElt = document.getElementById("search");
     
     // Adding a manager for the click event
     boutonElt.addEventListener("click", function () {
-      /* console.log("clic");*/
-
       var form = document.querySelector("form");
       // Displays all data entered or selected
       form.addEventListener("submit", function (e) {
