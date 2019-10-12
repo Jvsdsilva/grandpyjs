1b4622d1f0b1dc8cbf9e3a6d60aa671a410dabf8
Joana Silva <mfjotaa@hotmail.com>
2019-10-12 19:39:35 +0200

    corr

--- grandpyapp/static/js/ajax.js
+++ grandpyapp/static/js/ajax.js
@@ -8,11 +8,12 @@ function ajaxGet(url, callback) {
             callback(req.responseText);
         } 
         else {
-            alert("Any results!! Try again, please.");
+            
             console.error(req.status + " " + req.statusText + " " + url);
         }
     });
     req.addEventListener("error", function () {
+        alert("Any results!! Try again, please.");
         console.error("Erreur réseau avec l'URL " + url);
     });
     req.send(null);
